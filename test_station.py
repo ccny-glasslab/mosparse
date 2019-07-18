4c4713613f2b2d32bb363b9d3e51b04985984e08
Theo HIRES-2019 <theosch@frankenstein.engr.ccny.cuny.edu>
2019-07-18 11:58:59 -0400

    Added documentation strings and shifted reading over 1 column to fix errors

--- test_station.py
+++ test_station.py
@@ -1,13 +1,112 @@
 from dateutil.tz import tzutc 
 import datetime
 import mav_parse as mp
+import pandas as pd
 
 def test_get_header():
     header = mp.get_header(' KMVY   GFS MOS GUIDANCE    1/04/2019  1800 UTC                      \n')
     truth = {'station':'KMVY', 'model':'GFS MOS GUIDANCE', 'runtime':datetime.datetime(2019,1,4,18,0, tzinfo=tzutc())}
     assert header==truth
+    
 def test_get_fntime():
     row1 = ' DT /JAN   2                  /JAN   3                /JAN   4       \n'
     row2 = ' HR   00 03 06 09 12 15 18 21 00 03 06 09 12 15 18 21 00 03 06 12 18 \n'
     fntimes = mp.get_fntime(row1,row2,{'station':'KSTS', 'model':'GFS MOS GUIDANCE', 'runtime':datetime.datetime(2019,1,1,18,0, tzinfo=tzutc())}) 
-    truth = 
+    truth = [datetime.datetime(2019, 1, 2, 0, 0), datetime.datetime(2019, 1, 2, 3, 0), datetime.datetime(2019, 1, 2, 6, 0), datetime.datetime(2019, 1, 2, 9, 0), datetime.datetime(2019, 1, 2, 12, 0), datetime.datetime(2019, 1, 2, 15, 0), datetime.datetime(2019, 1, 2, 18, 0), datetime.datetime(2019, 1, 2, 21, 0), datetime.datetime(2019, 1, 3, 0, 0), datetime.datetime(2019, 1, 3, 3, 0), datetime.datetime(2019, 1, 3, 6, 0), datetime.datetime(2019, 1, 3, 9, 0), datetime.datetime(2019, 1, 3, 12, 0), datetime.datetime(2019, 1, 3, 15, 0), datetime.datetime(2019, 1, 3, 18, 0), datetime.datetime(2019, 1, 3, 21, 0), datetime.datetime(2019, 1, 4, 0, 0), datetime.datetime(2019, 1, 4, 3, 0), datetime.datetime(2019, 1, 4, 6, 0), datetime.datetime(2019, 1, 4, 12, 0), datetime.datetime(2019, 1, 4, 18, 0)]
+    assert fntimes==truth
+    
+def test_parse_row_all_values():
+    row5 = ' DPT  14 24 29 29 28 28 32 31 30 35 34 33 32 32 37 36 35 35 34 33 38 \n'
+    var,vals = mp.parse_row(row5)
+    truth_var = 'DPT'
+    truth_vals = ['14', '24', '29', '29', '28', '28', '32', '31', '30', '35', '34', '33', '32', '32', '37', '36', '35', '35', '34', '33', '38']
+    assert var==truth_var
+    assert vals==truth_vals
+
+def test_parse_row_6():
+    row3 =  ' P06         0     0     0     0     0     0     1     0     0  0  2 \n'
+    var,vals = mp.parse_row(row3)
+    truth_var = 'P06'
+    truth_vals = [None, None, '0', None, '0', None, '0', None, '0', None, '0', None, '0', None, '1', None, '0', None, '0', '0', '2']
+    assert var==truth_var
+    assert vals==truth_vals
+    
+def test_parse_row_12():
+    row3 =  ' N/X              28          57          32          55       34    \n'
+    var,vals = mp.parse_row(row3)
+    truth_var = 'N/X'
+    truth_vals = [None, None, None, None, '28', None, None, None, '57', None, None, None, '32', None, None, None, '55', None, None, '34', None]
+    assert var==truth_var
+    assert vals==truth_vals
+    
+def test_parse_row_6p():
+    row13 =  ' T06      0/ 0  0/ 4  0/ 0  0/ 0  0/ 0  0/ 1  0/ 0  0/ 0  0/ 0  0/ 0 \n'
+    var,vals = mp.parse_row(row13)
+    truth_var = 'T06'
+    truth_vals = [None, None, '0/ 0', None, '0/ 4', None, '0/ 0', None, '0/ 0', None, '0/ 0', None, '0/ 1', None, '0/ 0', None, '0/ 0', None, '0/ 0', None, '0/ 0']
+    assert var==truth_var
+    assert vals==truth_vals
+    
+def test_parse_row_12p():
+    row13 =  ' T12                  0/ 9        0/ 0        0/ 4        0/ 0  0/ 3 \n'
+    var,vals = mp.parse_row(row13)
+    truth_var = 'T12'
+    truth_vals = [None, None, None, None, None, None, '0/ 9', None, None, None, '0/ 0', None, None, None, '0/ 4', None, None, None, '0/ 0', None, '0/ 3']
+    assert var==truth_var
+    assert vals==truth_vals
+    
+def test_parse_station_empty():
+    station = []
+    truth = mp.parse_station(station)
+    expected = pd.DataFrame()
+    assert pd.DataFrame.equals(truth, expected)
+
+def test_get_rows():
+    header = None
+    station = [' KSTS   GFS MOS GUIDANCE    1/01/2019  1800 UTC                      \n',
+ ' DT /JAN   2                  /JAN   3                /JAN   4       \n',
+ ' HR   00 03 06 09 12 15 18 21 00 03 06 09 12 15 18 21 00 03 06 12 18 \n',
+ ' N/X              28          57          32          55       34    \n',
+ ' TMP  60 48 38 34 31 32 43 54 55 43 38 36 35 34 41 52 53 45 40 36 44 \n',
+ ' DPT  14 24 29 29 28 28 32 31 30 35 34 33 32 32 37 36 35 35 34 33 38 \n',
+ ' CLD  FW CL CL CL FW SC FW CL FW SC CL SC BK BK SC SC BK BK BK OV OV \n',
+ ' WDR  08 31 31 00 33 09 00 00 14 15 14 00 32 35 02 00 16 15 17 32 16 \n',
+ ' WSP  05 03 01 00 01 01 00 00 02 03 02 00 01 01 01 00 04 03 01 01 01 \n',
+ ' P06         0     0     0     0     0     0     1     0     0  0  2 \n',
+ ' P12               0           1           0           2        1    \n',
+ ' Q06         0     0     0     0     0     0     0     0     0  0  0 \n',
+ ' Q12               0           0           0           0        0    \n',
+ ' T06      0/ 0  0/ 4  0/ 0  0/ 0  0/ 0  0/ 1  0/ 0  0/ 0  0/ 0  0/ 0 \n',
+ ' T12                  0/ 9        0/ 0        0/ 4        0/ 0  0/ 3 \n',
+ ' POZ   1  1  3  2  1  2  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0 \n',
+ ' POS  18 21  8 19 25 12  0  2  0  0  0  0  0  0  0  0  0  0  0  0  0 \n',
+ ' TYP   R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R \n',
+ ' CIG   8  8  8  8  2  1  8  8  8  8  8  1  1  1  2  8  8  8  8  1  1 \n',
+ ' VIS   7  7  7  7  7  1  1  6  7  7  3  2  2  1  1  6  7  5  3  1  1 \n',
+ ' OBV   N  N  N  N  N FG FG HZ  N HZ BR BR FG FG FG HZ  N BR BR FG FG \n']
+    return mp.get_rows(header, station)
+print(test_get_rows())
+
+
+station = [' KSTS   GFS MOS GUIDANCE    1/01/2019  1800 UTC                      \n',
+ ' DT /JAN   2                  /JAN   3                /JAN   4       \n',
+ ' HR   00 03 06 09 12 15 18 21 00 03 06 09 12 15 18 21 00 03 06 12 18 \n',
+ ' N/X              28          57          32          55       34    \n',
+ ' TMP  60 48 38 34 31 32 43 54 55 43 38 36 35 34 41 52 53 45 40 36 44 \n',
+ ' DPT  14 24 29 29 28 28 32 31 30 35 34 33 32 32 37 36 35 35 34 33 38 \n',
+ ' CLD  FW CL CL CL FW SC FW CL FW SC CL SC BK BK SC SC BK BK BK OV OV \n',
+ ' WDR  08 31 31 00 33 09 00 00 14 15 14 00 32 35 02 00 16 15 17 32 16 \n',
+ ' WSP  05 03 01 00 01 01 00 00 02 03 02 00 01 01 01 00 04 03 01 01 01 \n',
+ ' P06         0     0     0     0     0     0     1     0     0  0  2 \n',
+ ' P12               0           1           0           2        1    \n',
+ ' Q06         0     0     0     0     0     0     0     0     0  0  0 \n',
+ ' Q12               0           0           0           0        0    \n',
+ ' T06      0/ 0  0/ 4  0/ 0  0/ 0  0/ 0  0/ 1  0/ 0  0/ 0  0/ 0  0/ 0 \n',
+ ' T12                  0/ 9        0/ 0        0/ 4        0/ 0  0/ 3 \n',
+ ' POZ   1  1  3  2  1  2  0  0  0  0  0  0  0  0  0  0  0  0  0  0  0 \n',
+ ' POS  18 21  8 19 25 12  0  2  0  0  0  0  0  0  0  0  0  0  0  0  0 \n',
+ ' TYP   R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R  R \n',
+ ' CIG   8  8  8  8  2  1  8  8  8  8  8  1  1  1  2  8  8  8  8  1  1 \n',
+ ' VIS   7  7  7  7  7  1  1  6  7  7  3  2  2  1  1  6  7  5  3  1  1 \n',
+ ' OBV   N  N  N  N  N FG FG HZ  N HZ BR BR FG FG FG HZ  N BR BR FG FG \n']
+
