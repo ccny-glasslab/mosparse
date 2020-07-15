566055f5a131753b93497092a79c11aa923aa3b7
hannah <story645@gmail.com>
2020-07-15 03:23:24 +0000

    context manager

--- mosparse/mavreader.py
+++ mosparse/mavreader.py
@@ -1,87 +1,40 @@
+import io
 import gzip
-import zlib
 from pathlib import Path
+import zlib
+
+from unlzw import unlzw
 
 class MavReader:
     ''' Opens AVNMAV file and returns stream of data
     '''
     def __init__(self, filepath):
         self.filepath = Path(filepath)
-        self.stream = "hello hello"
         if not self.filepath.exists():
             raise FileNotFoundError(f"{self.filepath}")
+        self.stream = None
         
     def __enter__(self):
         if self.filepath.suffix == '.gz':
-            #self._get_stations_gz()
+            self.stream = gzip.open(self.filepath,'r')     
         elif self.filepath.suffix == '.Z':
-            #self._get_stations_z()
+            file_obj = open(self.filepath, 'rb')
+            compressed_data = file_obj.read()
+            self.stream = io.BytesIO(unlzw(compressed_data))
+            file_obj.close()
         else:
-            #self._get_stations_other()
+            self.stream = open(self.filepath, 'rb') 
         return self.stream
     
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        pass
+        self.stream.close()
             
     
-    def _get_stations_other(self):
+    def get_stations_z(self):
         '''
-        Opens and reads files that are not .Z and .gz formats.
-
-        Parameters
-        -----------
-        path : string 
-            Directions to the file you are going to use.
-
-        Returns
-        --------
-        station : list of strings
-            The data collected by the model.
+        Opens and reads files that are of the format .Z..
         '''
-
-        with open(path, 'rb') as f:
-            self.f = f
-    
-    def _get_stations_z(self):
-        '''
-        Opens and reads files that are of the format .Z.
-
-        Parameters
-        -----------
-        path : string 
-            Directions to the file you are going to use.
-
-        Returns
-        --------
-        station : list of strings
-            The data collected by the model.
-        '''
-        from unlzw import unlzw
-        with open(path, 'rb') as fh:
-            compressed_data = fh.read()
-            uncompressed_data = unlzw(compressed_data)
-            uncompressed_data = uncompressed_data.split(b'\n')
-            for i in range(len(uncompressed_data)):
-                uncompressed_data[i] = uncompressed_data[i] + b'\n'
-           # print(uncompressed_data[0:20])
-            #uncompressed_data =           uncompressed_data.decode(errors='ignore').split('\n')
-        #for i in range(len(uncompressed_data)):
-        #    uncompressed_data[i] = (uncompressed_data[i] + '\n').encode()
-        return get_main_stations(uncompressed_data)
+   
+       
         
-    def self._get_stations_gz(self):
-        '''
-        Opens and reads files that are of the format .gz
 
-        Parameters
-        ------------
-        path : string 
-            Directions to the file you are going to use.
-
-        Returns
-        --------
-        station : list of strings
-            The data collected by the model.
-        '''
-        with gzip.open(path,'r') as f:
-            return get_main_stations(f)
